40fea0c3409d558f5501ac5f2ac904115ee5375f
iceCreamCoder <jared.web@live.com>
2019-11-07 16:05:06 -0800

    added files for kitten searcher

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import CardList from './CardList';
+import {kittens} from './kittens';
+import SearchBox from './SearchBox';
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+;class App extends Component  {
+
+    constructor() {
+        super() 
+            this.state = {
+                kittens: kittens,
+                searchfield: ''
+            }
+    }
+
+
+    onSearchChange = (event) => {
+       this.setState({
+           searchfield: event.target.value
+       })
+
+
+    }
+
+
+
+
+    render() {
+        const filteredKittens = this.state.kittens.filter(kittens => {
+            return kittens.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        return (
+            <div className="tc">
+                <h1 className="f1">Crazy Kittens!</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList kittens={filteredKittens} />
+            </div>
+        )
+    }
+
 }
 
 export default App;
---
+++ src/kittens.js
@@ -0,0 +1,62 @@
+export const kittens = [
+  {
+    id: 1,
+    name: 'Professor PersnicketyClaw',
+    username: 'theProfMeow',
+    email: 'pouncer-College20@claws.net'
+  },
+  {
+    id: 2,
+    name: 'Sally MeowWoeth',
+    username: 'silkyfeline',
+    email: 'quickfeelin@claws.cat'
+  },
+  {
+    id: 3,
+    name: 'Mademoiselle VigilantPounce the second',
+    username: 'SamCatterson',
+    email: 'pouncersOnCall@claws.biz'
+  },
+  {
+    id: 4,
+    name: 'Cheddar Claws',
+    username: 'ClawsHouse',
+    email: 'mice&cheeseRnice@claws.cat'
+  },
+  {
+    id: 5,
+    name: 'Cattailia MouseHunter',
+    username: 'Katterson',
+    email: 'MouseHunter@claws.cat'
+  },
+  {
+    id: 6,
+    name: 'Ferteh Clawsburn',
+    username: 'clawsBorn',
+    email: 'catfoodisnice@claws.cat'
+  },
+  {
+    id: 7,
+    name: 'Rufflton Startled',
+    username: 'largeEyes',
+    email: 'MrScaredAllTimes@claws.cat'
+  },
+  {
+    id: 8,
+    name: 'Kneader Meows the V',
+    username: 'kneading_time',
+    email: 'agilityPaws@claws.cat'
+  },
+  {
+    id: 9,
+    name: 'Catlanta SmallHider',
+    username: 'BasketsRbest',
+    email: 'Baskets!!@claws.cat'
+  },
+  {
+    id: 10,
+    name: 'HairFlex Spyer',
+    username: 'SpyingOnYou',
+    email: 'hidden@claws.cat'
+  }
+];

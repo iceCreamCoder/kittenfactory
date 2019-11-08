3a14d77f74cef0fba3e4d52a8bf13cd2ca9f05cf
iceCreamCoder <jared.web@live.com>
2019-11-07 16:34:35 -0800

    Updates

---
+++ precache-manifest.5429dc0119185aa66b5b5bf84c6dc403.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6592b650b21cd5bd2a263227ccb024be",
+    "url": "/kittenfactory/index.html"
+  },
+  {
+    "revision": "e9a94988c834c9915470",
+    "url": "/kittenfactory/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "334c43def8528fbab21c",
+    "url": "/kittenfactory/static/css/main.c0515f36.chunk.css"
+  },
+  {
+    "revision": "e9a94988c834c9915470",
+    "url": "/kittenfactory/static/js/2.0dc0b19e.chunk.js"
+  },
+  {
+    "revision": "334c43def8528fbab21c",
+    "url": "/kittenfactory/static/js/main.7d611a24.chunk.js"
+  },
+  {
+    "revision": "3459bf47f0e3e2cfa44b",
+    "url": "/kittenfactory/static/js/runtime-main.66184e70.js"
+  }
+]);
